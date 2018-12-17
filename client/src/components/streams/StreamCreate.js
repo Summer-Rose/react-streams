ed1d4c19c0f75cd9537cb83a627ceedc2c03e0bb
Summer Brochtrup <sbrochtrup@gmail.com>
2018-12-17 09:29:19 +0100

    create new streams

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from  'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,8 +25,8 @@ class StreamCreate extends React.Component {
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -49,7 +51,15 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+//wire up both reduxform and connect function
+const formWrapped = reduxForm({
   form: 'StreamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
+
+// export default reduxForm({
+//   form: 'StreamCreate',
+//   validate
+// })(StreamCreate);
